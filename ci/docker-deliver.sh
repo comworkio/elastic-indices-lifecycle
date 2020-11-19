7335de3d01f49d7d98b294a19c253b247475a1ee
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-19 14:47:03 +0100

    Fix docker tag

--- ci/docker-deliver.sh
+++ ci/docker-deliver.sh
@@ -4,7 +4,7 @@ REPO_PATH="/home/centos/elastic-indices-lifecycle/"
 VERSION="1.0"
 
 deliver() {
-  docker tag "${1}" comworkio/elastic-indices-lifecycle
+  docker tag comworkio/elastic-indices-lifecycle "${1}"
   docker push comworkio/elastic-indices-lifecycle:$1
 }
 
