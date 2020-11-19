43572fa3254529d505999fbbf5ba4276ff319cc7
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-19 22:58:48 +0100

    Handle subpath

--- rollup.py
+++ rollup.py
@@ -67,7 +67,7 @@ LOG_LEVEL = conf['log_level']
 DATE_FORMAT = conf['date_format']
 
 if is_not_empty(ES_USER) and is_not_empty(ES_PASS) and is_not_empty(ES_SUBPATH):
-    es = Elasticsearch("{}://{}:{}@{}/{}".format(ES_SCHEME, ES_USER, ES_PASS, ES_HOSTS, ES_PORT, ES_SUBPATH))
+    es = Elasticsearch("{}://{}:{}@{}:{}/{}".format(ES_SCHEME, ES_USER, ES_PASS, ES_HOSTS, ES_PORT, ES_SUBPATH))
 elif is_not_empty(ES_USER) and is_not_empty(ES_PASS)
     es = Elasticsearch(ES_HOSTS, http_auth=(ES_USER, ES_PASS), scheme = ES_SCHEME, port = ES_PORT)
 elif is_not_empty(ES_SUBPATH):
