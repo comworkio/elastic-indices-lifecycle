539cb6baea5c6a7785534e02153c03282eba01b3
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-19 14:50:11 +0100

    Fix docker tag

--- ci/docker-deliver.sh
+++ ci/docker-deliver.sh
@@ -3,17 +3,14 @@
 REPO_PATH="/home/centos/elastic-indices-lifecycle/"
 VERSION="1.0"
 
-deliver() {
-  docker tag comworkio/elastic-indices-lifecycle "${1}"
-  docker push comworkio/elastic-indices-lifecycle:$1
+tag() {
+  docker tag "${1}" comworkio/elastic-indices-lifecycle:latest
 }
 
 cd "${REPO_PATH}" && git pull origin master || :
 git push github master 
 
 docker-compose build
-deliver "latest"
-deliver "${VERSION}"
-deliver "${VERSION}-${CI_COMMIT_SHORT_SHA}"
-
+tag "${VERSION}"
+tag "${VERSION}-${CI_COMMIT_SHORT_SHA}"
 docker-compose push
