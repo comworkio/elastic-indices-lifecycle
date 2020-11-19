94c7c55e5e474bef2c07b1be84e1abd7f41d8703
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-19 14:35:11 +0100

    Refactoring CI

--- ci/docker-deliver.sh
+++ ci/docker-deliver.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 REPO_PATH="/home/centos/elastic-indices-lifecycle/"
+VERSION="1.0"
+
+deliver() {
+  docker tag comworkio/elastic-indices-lifecycle:$1
+  docker push comworkio/elastic-indices-lifecycle:$1
+}
 
 cd "${REPO_PATH}" && git pull origin master || :
 git push github master 
 
 docker-compose build
-
-docker tag comworkio/elastic-indices-lifecycle:latest
-docker tag comworkio/elastic-indices-lifecycle:1.0
-docker tag comworkio/elastic-indices-lifecycle:1.0-$CI_COMMIT_SHORT_SHA
+deliver "latest"
+deliver "${VERSION}"
+deliver "${VERSION}-${CI_COMMIT_SHORT_SHA}"
 
 docker-compose push
