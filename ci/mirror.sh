871355d7168b9ec7764985acf6763874d6c7484d
Idriss Neumann <idriss.neumann@comwork.io>
2023-01-04 16:13:58 +0100

    Remove some mirrors and add dockerignore

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -5,6 +5,4 @@ REPO_PATH="${PROJECT_HOME}/elastic-indices-lifecycle/"
 cd "${REPO_PATH}" && git pull origin master || :
 git push github master 
 git push pgitlab master
-git push bitbucket master
-git push froggit master
 exit 0
